fc336354cc0b392a2715fa35feb94cf54d68989c
snopeks <creativesnopeks@gmail.com>
2017-10-13 09:26:29 -0700

    create hobby seed data

--- seed.js
+++ seed.js
@@ -3,13 +3,24 @@
 
 var db = require('./models');
 
-var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
-db.Campsite.create(new_campsite, function(err, campsite){
-  if (err){
-    return console.log("Error:", err);
+// db.Campsite.create(new_campsite, function(err, campsite){
+//   if (err){
+//     return console.log("Error:", err);
+//   }
+//
+//   console.log("Created new campsite", campsite._id)
+//   process.exit(); // we're all done! Exit the program.
+// })
+
+// HOBBY STUFF
+var new_hobby = {hobby: "hike"}
+
+db.HobbiesCollection.create(new_hobby, function(err, hobby){
+  if(err){
+    return console.log('Error', err);
   }
-
-  console.log("Created new campsite", campsite._id)
-  process.exit(); // we're all done! Exit the program.
+  console.log("created new hobby!", hobby._id)
+  process.exit();// we're all done! exit the program.
 })
